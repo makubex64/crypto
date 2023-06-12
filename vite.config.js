67722dbeb93c6590a9b55b7bd15125b17ed88e51
Jorge Adarmes <jorgeluismakubex@gmail.com>
2023-06-12 14:35:31 -0430

    modified

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/crypto",
   plugins: [react()],
+  base: "https://makubex64.github.io/crypto"
 })
  
